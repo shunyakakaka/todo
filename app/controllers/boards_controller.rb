bc5ae734ea4766d5f6b558a3b2f1a1bb7ccfd0f3
shunyasaitama <85655151+shunyasaitama@users.noreply.github.com>
2021-06-23 11:57:22 +0900

    詳細 編集

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,44 @@
 class BoardsController < ApplicationController
+
+  before_action :set_target_before, only: [:show, :edit, :update, :destroy]
   def index
+    @boards = Board.all
   end
+
+  def new
+    @board = Board.new()
+  end
+
+  def create
+    board = Board.create(board_params)
+    redirect_to board_path(board.id)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @board.update(board_params)
+
+    redirect_to board_path(@board.id)
+  end
+
+  def destroy
+    @board.delete
+
+    redirect_to boards_path
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
+  
+  def set_target_before
+    @board = Board.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'boards/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "boards", to: "boards#index"
+  resources :boards
 end
